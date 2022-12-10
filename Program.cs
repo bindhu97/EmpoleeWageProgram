6718f14ad34507fed6f90a8b6a67b83f739164b1
bindhu97 <binduvishwakarma97@gmail.com>
2022-12-10 22:10:41 +0530

    [Bindhu] Employee Present or Absent

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EmployeeWage;
+
+Console.WriteLine("Welcome to Employee Wage Computation Program");
+
+Console.WriteLine("Calling Employee Present or Absent");
+EmpPresentAbsent emppresentabsent = new EmpPresentAbsent();
+emppresentabsent.PresentAbsent();
