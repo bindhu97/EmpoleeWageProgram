d2030bb95b0a654b09866951033683bf0ef70c57
bindhu97 <binduvishwakarma97@gmail.com>
2022-12-10 22:34:09 +0530

    [Bindhu] Employee DailyWage

--- DailyWage.cs
+++ DailyWage.cs
@@ -6,21 +6,26 @@ using System.Threading.Tasks;
 
 namespace EmployeeWageProgram
 {
-    internal class EmpPresentAbsent
+    internal class DailyWage
     {
-        public void PresentAbsent()
+        public void EmployeeWage()
         {
+            int wagePerHour = 20;
+            int fullDayHour = 8;
+            int dailyWage = 0;
             Random rand = new Random();
             int employeeCheck = rand.Next(0, 2);
 
             if (employeeCheck == 1)
             {
+                dailyWage = fullDayHour * wagePerHour;
                 Console.WriteLine("Employee is present");
             }
             else
             {
                 Console.WriteLine("Employee is absent");
             }
+            Console.WriteLine("Employee total wage is: " + dailyWage);
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,8 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-using EmployeeWage;
+﻿using EmployeeWageProgram;
 
-Console.WriteLine("Welcome to Employee Wage Computation Program");
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        bool check = true;
+        while (check)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+            Console.WriteLine("Select Option:\n" +
+                "1) Find Employee Present or Absent\n" +
+                "2) Find Employee Daily Wage\n");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Calling Employee Present or Absent");
+                    EmpPresentAbsent emppresentabsent = new EmpPresentAbsent();
+                    emppresentabsent.PresentAbsent();
+                    break;
 
-Console.WriteLine("Calling Employee Present or Absent");
-EmpPresentAbsent emppresentabsent = new EmpPresentAbsent();
-emppresentabsent.PresentAbsent();
+                case 2:
+                    Console.WriteLine("Calling Employee Present or Absent");
+                    DailyWage dailyWage = new DailyWage();
+                    dailyWage.EmployeeWage();
+                    break;
+                default:
+                    Console.WriteLine("Choose valid option");
+                    break;
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
