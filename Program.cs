aa1f757d33fe626f4e27a8248bbbb340b4cff933
bindhu97 <binduvishwakarma97@gmail.com>
2022-12-27 18:38:07 +0530

    [Bindhu]Employee Daily Wage

--- Program.cs
+++ Program.cs
@@ -1,41 +1,6 @@
 ﻿using EmployeeWageProgram;
 
-public class Program
-{
-    public static void Main(string[] args)
-    {
-        bool check = true;
-        while (check)
-        {
-            Console.WriteLine("Welcome to Employee Wage Computation Program");
-            Console.WriteLine("Select Option:\n" +
-                "1) Find Employee Present or Absent\n" +
-                "2) Find Employee Daily Wage\n");
-            int option = Convert.ToInt32(Console.ReadLine());
-            switch (option)
-            {
-                case 1:
-                    Console.WriteLine("Calling Employee Present or Absent");
-                    EmpPresentAbsent emppresentabsent = new EmpPresentAbsent();
-                    emppresentabsent.PresentAbsent();
-                    break;
-
-                case 2:
-                    Console.WriteLine("Calling Employee Present or Absent");
-                    DailyWage dailyWage = new DailyWage();
-                    dailyWage.EmployeeWage();
-                    break;
-                default:
-                    Console.WriteLine("Choose valid option");
-                    break;
-            }
-        }
-    }
-}
-
-
-
-
-
-
+EmployeeWageCompute EmpPresentAbsent = new EmployeeWageCompute();
+//EmpPresentAbsent.EmployeePresentAbsent();
+EmployeeWageCompute.DailyEmployeeWage();
 
