477b19c694df3fdafc9c29bc1b529d703a2fff03
bindhu97 <binduvishwakarma97@gmail.com>
2022-12-27 23:14:23 +0530

    [Bindhu]Save total wage for each company

--- EmployeeWageCompute.cs
+++ EmployeeWageCompute.cs
@@ -205,5 +205,53 @@ namespace EmployeeWageProgram
             int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Total Emp Wage for Company:" + company + "is:" + totalEmpWage);
         }
+        public class EmpWageForMultipleCompany
+        {
+            public const int fullTime = 2;
+            public const int partTime = 1;
+
+            private string company;
+            private int empRatePerHrs;
+            private int numberOfWorkingDays;
+            private int maxHrsPerMonth;
+            private int totalEmpWage;
+            public EmpWageForMultipleCompany(string company, int empRatePerHrs, int numberOfWorkingDay, int maxHrsPerMonth)
+            {
+                this.company = company;
+                this.empRatePerHrs = empRatePerHrs;
+                this.numberOfWorkingDays = numberOfWorkingDay;
+                this.maxHrsPerMonth = maxHrsPerMonth;
+            }
+            public void ComputeEmpWage()
+            {
+                int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+                while (totalEmpHrs != this.maxHrsPerMonth && totalWorkingDays < this.numberOfWorkingDays)
+                {
+                    totalWorkingDays++;
+                    Random random = new Random();
+                    int empcheck = random.Next(0, 3);
+                    switch (empcheck)
+                    {
+                        case fullTime:
+                            empHrs = 8;
+                            break;
+                        case partTime:
+                            empHrs = 4;
+                            break;
+                        default:
+                            empHrs = 0;
+                            break;
+                    }
+                    totalEmpHrs += empHrs;
+                    Console.WriteLine("Days " + totalWorkingDays + "EmpHrs: " + totalEmpHrs);
+                }
+                totalEmpWage = totalEmpHrs * this.empRatePerHrs;
+                Console.WriteLine("Total Emp Wage for company: " + company + " is " + totalEmpWage);
+            }
+            public string toString()
+            {
+                return "Total Emp Wage for campny: " + this.company + " is " + this.totalEmpWage;
+            }
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,12 +1,25 @@
 ﻿using EmployeeWageProgram;
+using static EmployeeWageProgram.EmployeeWageCompute;
 
-EmployeeWageCompute EmpPresentAbsent = new EmployeeWageCompute();
-//EmpPresentAbsent.EmployeePresentAbsent();
-//EmployeeWageCompute.DailyEmployeeWage();
-//EmployeeWageCompute.EmployeePartTimeWage();
-//EmployeeWageCompute.EmployeeWageSwitchCase();
-//EmployeeWageCompute.MonthlyWage();
-//EmployeeWageCompute.TotalWorkingHours();
-//EmployeeWageCompute.Refactor();
-EmployeeWageCompute.EmployeeWageMultipleCompany("Dmart", 20, 20, 100);
-EmployeeWageCompute.EmployeeWageMultipleCompany("Reliance", 10, 20, 100);
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        EmployeeWageCompute EmpPresentAbsent = new EmployeeWageCompute();
+        //EmpPresentAbsent.EmployeePresentAbsent();
+        //EmployeeWageCompute.DailyEmployeeWage();
+        //EmployeeWageCompute.EmployeePartTimeWage();
+        //EmployeeWageCompute.EmployeeWageSwitchCase();
+        //EmployeeWageCompute.MonthlyWage();
+        //EmployeeWageCompute.TotalWorkingHours();
+        //EmployeeWageCompute.Refactor();
+        //EmployeeWageCompute.EmployeeWageMultipleCompany("Dmart", 20, 20, 100);
+        //EmployeeWageCompute.EmployeeWageMultipleCompany("Reliance", 10, 20, 100);
+        EmpWageForMultipleCompany Dmart = new EmpWageForMultipleCompany("Dmart", 20, 20, 100);
+        EmpWageForMultipleCompany Reliance = new EmpWageForMultipleCompany("Reliance", 10, 20, 100);
+        Dmart.ComputeEmpWage();
+        Console.WriteLine(Dmart.ToString());
+        Reliance.ComputeEmpWage();
+        Console.WriteLine(Reliance.ToString());
+    }
+}
