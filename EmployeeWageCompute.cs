9484c15c528ac3b293dd0f96a6e9a12cac203392
bindhu97 <binduvishwakarma97@gmail.com>
2022-12-27 16:10:10 +0530

    [Bindhu]Employee Present or Absent

--- EmployeeWageCompute.cs
+++ EmployeeWageCompute.cs
@@ -6,26 +6,21 @@ using System.Threading.Tasks;
 
 namespace EmployeeWageProgram
 {
-    internal class DailyWage
+    internal class EmployeeWageCompute
     {
-        public void EmployeeWage()
+        public void PresentAbsent()
         {
-            int wagePerHour = 20;
-            int fullDayHour = 8;
-            int dailyWage = 0;
             Random rand = new Random();
             int employeeCheck = rand.Next(0, 2);
 
             if (employeeCheck == 1)
             {
-                dailyWage = fullDayHour * wagePerHour;
                 Console.WriteLine("Employee is present");
             }
             else
             {
                 Console.WriteLine("Employee is absent");
             }
-            Console.WriteLine("Employee total wage is: " + dailyWage);
         }
     }
 }
