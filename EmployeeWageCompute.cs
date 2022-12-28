6480c4c3955b43a59b3d1eb8873be588cbb959be
bindhu97 <binduvishwakarma97@gmail.com>
2022-12-28 20:37:06 +0530

    [Bindhu]Adding:Employee Monthly Wages

--- EmployeeWageCompute.cs
+++ EmployeeWageCompute.cs
@@ -11,25 +11,31 @@ namespace EmployeeWageProgram
         public const int Full_Time = 1;
         public const int Part_Time = 2;
         public const int Emp_Per_Hours = 20;
+        public const int Num_Of_Working_Days = 20;
         public static void EmpWage()
         {
-            int empHrs, empWage;
-            Random random = new Random();
-            int check = random.Next(0, 3);
-            switch (check)
+            int empHrs, empWage, totalEmpWage = 0;
+            for (int day = 1; day <= Num_Of_Working_Days; day++)
             {
-                case Full_Time:
-                    empHrs = 8;
-                    break;
-                case Part_Time:
-                    empHrs = 4;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
+                Random random = new Random();
+                int check = random.Next(0, 3);
+                switch (check)
+                {
+                    case Full_Time:
+                        empHrs = 8;
+                        break;
+                    case Part_Time:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * Emp_Per_Hours;
+                totalEmpWage = totalEmpWage + empWage;
+                Console.WriteLine("Employee Wage : " + empWage);
             }
-            empWage = empHrs * Emp_Per_Hours;
-            Console.WriteLine("Employee Wage : " + empWage);
+            Console.WriteLine("Total Employee Wages : " + totalEmpWage);
         }
     }
 }
