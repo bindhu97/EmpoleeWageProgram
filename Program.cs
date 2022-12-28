8a08030e1222186a9c8f207dadce344802e13536
bindhu97 <binduvishwakarma97@gmail.com>
2022-12-28 12:30:30 +0530

    [Bindhu]Adding:Employee Daily Wage

--- Program.cs
+++ Program.cs
@@ -2,11 +2,6 @@
 using EmployeeWageProgram;
 using static EmployeeWageProgram.EmployeeWageCompute;
 
-internal class Program
-{
-    private static void Main(string[] args)
-    {
-        EmployeeWageCompute EmpPresentAbsent = new EmployeeWageCompute();
-        EmpPresentAbsent.EmployeePresentAbsent;
-    }
-}
+EmployeeWageCompute EmpPresentAbsent = new EmployeeWageCompute();
+//EmpPresentAbsent.EmployeePresentAbsent;
+EmployeeWageCompute.DailyEmployeeWage();
