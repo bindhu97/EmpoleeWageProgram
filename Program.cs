bae08388ac7a6ae12d93cfe167d29d0bc6e8369b
bindhu97 <binduvishwakarma97@gmail.com>
2022-12-28 12:06:02 +0530

    [Bindhu]Adding:Employee Present or Absent

--- Program.cs
+++ Program.cs
@@ -1,29 +1,12 @@
 ﻿using EmployeeWageProgram;
+using EmployeeWageProgram;
 using static EmployeeWageProgram.EmployeeWageCompute;
 
 internal class Program
 {
-    static void Main(string[] args)
+    private static void Main(string[] args)
     {
         EmployeeWageCompute EmpPresentAbsent = new EmployeeWageCompute();
-        //EmpPresentAbsent.EmployeePresentAbsent();
-        //EmployeeWageCompute.DailyEmployeeWage();
-        //EmployeeWageCompute.EmployeePartTimeWage();
-        //EmployeeWageCompute.EmployeeWageSwitchCase();
-        //EmployeeWageCompute.MonthlyWage();
-        //EmployeeWageCompute.TotalWorkingHours();
-        //EmployeeWageCompute.Refactor();
-        //EmployeeWageCompute.EmployeeWageMultipleCompany("Dmart", 20, 20, 100);
-        //EmployeeWageCompute.EmployeeWageMultipleCompany("Reliance", 10, 20, 100);
-        //EmpWageForMultipleCompany Dmart = new EmpWageForMultipleCompany("Dmart", 20, 20, 100);
-        //EmpWageForMultipleCompany Reliance = new EmpWageForMultipleCompany("Reliance", 10, 20, 100);
-        //Dmart.ComputeEmpWage();
-        //Console.WriteLine(Dmart.ToString());
-        //Reliance.ComputeEmpWage();
-        //Console.WriteLine(Reliance.ToString());
-        EmpWageBuilderArray empWageBuilderArray = new EmpWageBuilderArray();
-        empWageBuilderArray.addCompanyEmpWage("Dmart", 20, 20, 100);
-        empWageBuilderArray.addCompanyEmpWage("Reliance", 10, 20, 100);
-        empWageBuilderArray.computeEmpWage();
+        EmpPresentAbsent.EmployeePresentAbsent;
     }
 }
